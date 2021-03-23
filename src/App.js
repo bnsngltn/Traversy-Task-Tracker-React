b16809719ed2c96d415ee9d0750c27e18d6b501d
bnsngltn <yuushurou@gmail.com>
2021-03-23 10:12:20 +0800

    Added a bunch of features while following along

--- src/App.js
+++ src/App.js
@@ -23,12 +23,31 @@ function App() {
         day: "Feb 7",
         reminder: false
     },
-])
+  ])
+
+  // Callback function basically says that
+  // if task.id matches the id of the clicked one, remove it
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  // I implemented by switching logic here
+  const toggleReminder = (id) => {
+    setTasks(tasks.map(task => {
+      if (task.id === id) {
+        task.reminder = !(task.reminder)
+      }
+      return task
+    }))
+  }
 
   return (
     <div className="container">
       <Header title="Task Tracker"/>
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? 
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 
+        "No Tasks to Show"}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,15 +1,13 @@
 import Task from "./Task"
 
-export const Tasks = ({ tasks }) => {
-    
-
+export const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
             {tasks.map(task => {
                 return (
-                    <Task key={task.id} task={task}/>
+                    <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
                 )
-            })}   
+            })}
         </>
     )
 }
